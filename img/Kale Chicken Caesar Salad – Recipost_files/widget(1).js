88067b4c5c47be45e31c2df4f7e4a39b14223fec
Karen Mitchell <karen1752000@yahoo.com>
2018-05-29 14:51:08 -0400

    Photos now have transitionto them

---
+++ img/Kale Chicken Caesar Salad – Recipost_files/widget(1).js
@@ -0,0 +1,6 @@
+var _d_site = _d_site || "0DF071652C39F77B7A9F48C6";
+(function(p, r, i, v, y) {
+  p[i] = p[i] || function() { (p[i].q = p[i].q || []).push(arguments) };
+  v = r.createElement('script'); v.async = 1; v.src = 'https://widget.privy.com/assets/widget.js';
+  y = r.getElementsByTagName('script')[0]; y.parentNode.insertBefore(v, y);
+})(window, document, 'Privy');
